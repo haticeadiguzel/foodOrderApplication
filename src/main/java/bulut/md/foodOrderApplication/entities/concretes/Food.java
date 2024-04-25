cd443538e2c8aa7fd2ec7260424af51c34325108
haticeadiguzel <adiguzell.hatice@gmail.com>
2024-04-26 01:38:29 +0300

    added: Model classes for Food and Order

---
+++ src/main/java/bulut/md/foodOrderApplication/entities/concretes/Food.java
@@ -0,0 +1,32 @@
+package bulut.md.foodOrderApplication.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "foods")
+public class Food {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "food_name")
+    private String foodName;
+
+    @Column(name = "food_price")
+    private BigDecimal foodPrice;
+
+    @OneToMany(mappedBy = "food")
+    @JsonIgnore
+    private List<Order> orders;
+}
---
+++ src/main/java/bulut/md/foodOrderApplication/entities/concretes/Order.java
@@ -0,0 +1,25 @@
+package bulut.md.foodOrderApplication.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne()
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    @Column(name = "customer_name")
+    private String customerName;
+}
