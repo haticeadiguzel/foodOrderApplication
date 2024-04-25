de47d86af181f63069be5afcf864eccf583054ae
haticeadiguzel <adiguzell.hatice@gmail.com>
2024-04-26 00:17:00 +0300

    created: spring maven project

---
+++ src/test/java/bulut/md/foodOrderApplication/FoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package bulut.md.foodOrderApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
