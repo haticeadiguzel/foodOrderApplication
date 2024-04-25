322cda22cdef10cfa63d52dfc8f58aa157a96b7d
haticeadiguzel <adiguzell.hatice@gmail.com>
2024-04-26 02:54:32 +0300

    updated: model mapper bean

--- src/main/java/bulut/md/foodOrderApplication/FoodOrderApplication.java
+++ src/main/java/bulut/md/foodOrderApplication/FoodOrderApplication.java
@@ -1,7 +1,9 @@
 package bulut.md.foodOrderApplication;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FoodOrderApplication {
@@ -10,4 +12,8 @@ public class FoodOrderApplication {
 		SpringApplication.run(FoodOrderApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 }
